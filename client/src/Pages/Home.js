6722f2778a02a89d84fd7d855f778476cf269d20
Brian Gearty <geartybrian@gmail.com>
2021-04-29 14:21:20 -0400

    Home page

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "../style/home.css";
+// import HomeCards from "../Components/homeCards";
+// import Subscribe from "../Components/subscribe";
+// import Video from "../Components/video";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+function Home() {
+    return (
+        <div> Home Page
+            <div class="container-fluid">
+                {/* <Video /> */}
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            {/* <HomeCards /> */}
+            <br />
+
+            {/* <Subscribe /> */}
+            <br />
+        </div>
+
+    );
+}
+
+
+
+export default Home;
